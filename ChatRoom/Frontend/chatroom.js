0916efcd2f4782d722e8b37ad287246f39be53d4
KK0603 <yangweijun1989@163.com>
2015-05-20 14:59:07 +0800

    finish interfaces design

--- ChatRoom/Frontend/chatroom.js
+++ ChatRoom/Frontend/chatroom.js
@@ -1,23 +1,153 @@
 ;
-(function(){
+(function($) {
+	var websocket = {};
+	// var websocket = new Websocket('');
+	var user = '';
 
-// 界面逻辑 
-function showMessage(userName,message){}
+	/**
+	 * 用户登录
+	 */
+	$('#submit').click(submitClickHandler);
+	$('#loginName').focus();
 
-function addUser(userName){}
+	function submitClickHandler() {
+		var name = $('#loginName').val();
+		if (name) {
+			$('#submit').text("登录中……");
+			$("#submit").addClass('ing');
+			$("#submit").unbind();
+			$.ajax({
+				url: 'register',
+				method: 'POST',
+				data: {
+					userName: name
+				},
+				error: function() {
+					$("#loginTips").text("服务器连接错误，请重试");
+					$("#loginTips").addClass('show');
+					$('#submit').text("进入聊天室");
+					$("#submit").removeClass('ing');
+					$('#submit').click(submitClickHandler);
+					$('#submit').text("登录成功！");
+					$("#submit").addClass('ok');
+					setTimeout(function() {
+						$("#loginDialog").addClass("fade")
+					}, 600);
+					setTimeout(function() {
+						$("#mainWrapper").addClass("show");
+						$('#inputText').focus();
+					}, 1000);
 
-function removeUser(userName){}
+				},
+				success: function(result) {
+					if (!result) {
+						$("#loginTips").text("服务器返回错误，请重试");
+						$("#loginTips").show();
+					} else {
+						switch (result.code) {
+							case 0:
+								/**
+								 * 打开websocket！！！
+								 */
+								websocket.open();
+								this.user = name;
+								$("#loginDialog").hide();
+								$("#mainWrapper").show();
+								$("#submit").removeClass('ing');
+								$('#submit').text("登录成功！");
+								$("#submit").addClass('ok');
+								if (result.userList && result.userList instanceof Array) {
+									var itemString = '';
+									for (var i = 0, length = result.userList.length; i < length; i++) {
+										itemString += ['<li class="userItem">', result.userList[i], '</li>'].join('');
+									}
+									$("#userList").append(itemString);
+								}
+								setTimeout($("#loginDialog").addClass("fade"), 500);
+							case -1:
+								$("#loginTips").text(result.text);
+								$("#loginTips").addClass('show');
+								$('#submit').text("进入聊天室");
+								$("#submit").removeClass('ing');
+								$('#submit').click(submitClickHandler);
+						}
+					}
 
-function genernicIcon(){}
+				}
+			});
+		} else {
+			$("#loginTips").text("不能使用空昵称，请重新输入");
+			$("#loginTips").addClass('show');
+		}
+	}
 
-// 业务逻辑
-function login(userName){}
+	$("#loginName").keydown(function(e) {
+		$("#loginTips").removeClass('show');
+		if(e.which == 13 || e.which == 10){
+			$("#submit").trigger("click");
+		}
+	});
 
-function sendMessage(message){}
+	$("#send").click(function() {
+		var text = $("#inputText").val().trim();
+		if (text) {
+			json = {
+				user: user,
+				data: text,
+				time: new Date().toString()
+			}
+			html = '<div class="messagebox"><div class="icon-l"></div><div class="message-l"><span class="user">useruseruseruseruseru1<span class="time">&nbsp;&nbsp;12:12</span></span><div class="contentwrapper"><pre class="content-l">'+text+'</pre></div></div></div>';
+			$("#messageShown")[0].innerHTML += html; 
+			var set = $('.messagebox');
+			var height = set[set.length-1].offsetTop;
+			$('#messageShown').animate({scrollTop: height}, 400); 
+			// websocket.send(JSON.stringify(json));
+		} else {
+			$("#textTips").text("不能发空消息");
+			$("#textTips").addClass('show');
+		}
+		$("#inputText").val('');
+	});
 
+	$("#inputText").keyup(function(e) {
+		var text = $("#inputText").val();
+		if(e.ctrlKey && e.which == 13 || e.which == 10){
+			$("#textTips").removeClass('show');	
+			$("#inputText").val(text+"\r\n");
+		}else if(e.which == 13 || e.which == 10){
+			$("#send").trigger("click");
+		}else{
+			$("#textTips").removeClass('show');			
+		}
 
+	});
 
-})();
+	websocket.onmessage = function(event) {
+		var data = event.data;
+		var html = '';
+		var style = '';
+		if (data && data.mine) {
+			style = '';
+		}
+		html = '<div class="messagebox"><div class="icon-r"></div><div class="message-r"><span class="user">useruseruseruseruseru1<span class="time">&nbsp;&nbsp;12:12</span></span><div class="contentwrapper"><pre class="content-r">sadfsadfasdfasdf</pre></div></div></div>';
+$("#messageShown").innerHTML += html;     };
+
+	$(window).unload(function() {
+		websocket.colse();
+		$.ajax({
+			url: 'unregister',
+			method: 'POST',
+			data: {
+				userName: name
+			},
+			error: function() {},
+			success: function(result) {}
+		});
+	});
+
+	function genernicIcon() {}
+
+})(jQuery);
 
 /* 	待实现
 	1.用户密码登录
--- ChatRoom/server.js
+++ ChatRoom/server.js
@@ -1,8 +1,11 @@
 // HTTP Server
 var http = require('http');
+var WebsocketServer = require('ws').server;
+
+
 var fs = require('fs');
-var httpServer = http.createServer(function(req,res){
-	fs.readFile('./Frontend/home.html',function(err,data){
+var httpServer = http.createServer(function(req, res) {
+	fs.readFile('./Frontend/home.html', function(err, data) {
 		data.pipe(res);
 	});
 });
@@ -11,3 +14,23 @@ httpServer.Listen(80);
 // Login 根据用户名查重
 
 // Message 进行websocket双向通信
+wss = new WebSocketServer({
+	port: 8080
+});
+// 定义广播方法data为直接发送数据
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		client.send(data);
+	});
+};
+// websocker server连接动作
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		var data = JSON.parse(message);
+		console.log('user: ' + data.user + 'text:' + data.text);
+		if (message) {
+			wws.broadcast(message);
+		}
+
+	});
+});
